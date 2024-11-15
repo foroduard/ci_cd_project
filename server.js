fe44b9069cf6b6d801030dc04e75ce088cdc9af9
Nizeyimana <frodouardnizeyimana@gmail.com>
2024-11-15 14:32:16 +0200

    setted

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app =express();
 app.get('/',(req, res) =>{
     res.send('Hello world');
 });
-
+app.get('/',(req, res) =>{
+    res.send('this is user route');
+});
 app.listen(3000, () =>{
     console.log('Server is running on port 3000')
 
