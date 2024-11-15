9c20cc7dadd631234a3f5efa95513ba0f9522fd1
Nizeyimana <frodouardnizeyimana@gmail.com>
2024-11-15 14:50:23 +0200

    added new branch

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 const app =express();
 
+
 app.get('/',(req, res) =>{
     res.send('Hello world');
 });
+
 app.get('/',(req, res) =>{
     res.send('this is user route');
 });
